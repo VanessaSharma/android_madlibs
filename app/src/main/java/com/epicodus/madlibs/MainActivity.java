e5e662077d99126611306f61239c318d982f514d
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-15 10:21:27 -0800

    Add toast, button, colors, and background.

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -2,12 +2,23 @@ package com.epicodus.madlibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mMadLibsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMadLibsButton = (Button) findViewById(R.id.madLibsButton);
+        mMadLibsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Ready to Play?", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
