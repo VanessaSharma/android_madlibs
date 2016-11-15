58377755ce7a935ddb06a7d910379aba23e44dc0
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-15 13:11:52 -0800

    Add new activity page MadLibsActivity.java.

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -1,24 +1,33 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mMadLibsButton;
+    private EditText mUsernameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mUsernameEditText = (EditText) findViewById(R.id.usernameEditText);
         mMadLibsButton = (Button) findViewById(R.id.madLibsButton);
         mMadLibsButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Ready to Play?", Toast.LENGTH_LONG).show();
-                }
-            });
+
+            @Override
+            public void onClick(View v) {
+                String location = mUsernameEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, MadLibsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
